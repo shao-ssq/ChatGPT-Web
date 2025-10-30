7702c27749865d4856656712805457c253bfe88b
燕双嘤 <1966799809@qq.com>
2025-10-30 10:38:49 +0800

    123123

---
+++ src/store/modules/settings/helper.ts
@@ -0,0 +1,30 @@
+import { ss } from '@/utils/storage'
+
+const LOCAL_NAME = 'settingsStorage'
+
+export interface SettingsState {
+  systemMessage: string
+  temperature: number
+  top_p: number
+}
+
+export function defaultSetting(): SettingsState {
+  return {
+    systemMessage: '你是天工 ChatExcel，来自 ChatBI。请仔细遵循用户的指示，并使用Markdown格式进行回复。',
+    temperature: 0.8,
+    top_p: 1,
+  }
+}
+
+export function getLocalState(): SettingsState {
+  const localSetting: SettingsState | undefined = ss.get(LOCAL_NAME)
+  return { ...defaultSetting(), ...localSetting }
+}
+
+export function setLocalState(setting: SettingsState): void {
+  ss.set(LOCAL_NAME, setting)
+}
+
+export function removeLocalState() {
+  ss.remove(LOCAL_NAME)
+}
---
+++ src/store/modules/user/helper.ts
@@ -0,0 +1,32 @@
+import { ss } from '@/utils/storage'
+
+const LOCAL_NAME = 'userStorage'
+
+export interface UserInfo {
+  avatar: string
+  name: string
+  description: string
+}
+
+export interface UserState {
+  userInfo: UserInfo
+}
+
+export function defaultSetting(): UserState {
+  return {
+    userInfo: {
+      avatar: '/avatar.jpg',
+      name: '天工 ChatExcel',
+      description: '源于 ChatBi',
+    },
+  }
+}
+
+export function getLocalState(): UserState {
+  const localSetting: UserState | undefined = ss.get(LOCAL_NAME)
+  return { ...defaultSetting(), ...localSetting }
+}
+
+export function setLocalState(setting: UserState): void {
+  ss.set(LOCAL_NAME, setting)
+}
